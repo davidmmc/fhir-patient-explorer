be3d123292932db90a124047b1a0c57324bd85e5
FCC0516 <FCC0516>
2018-03-20 11:29:15 -0500

    Update environment to load constants like appId

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  prod = environment.production;
+  clientId = environment.clientId;
 }
